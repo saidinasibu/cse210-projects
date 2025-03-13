b207b0ba24e8d6780fdc8de1f3281b7db533728f
saidinasibu <adjinasibusaidi@gmail.com>
2025-03-13 09:37:37 -0700

    Exercise 1 finished

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name?. ");
+        string first_name = Console.ReadLine();
+        
+        Console.WriteLine(first_name);
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name} {first_name} {first_name} ");
     }
 }
