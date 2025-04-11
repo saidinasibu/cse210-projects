03608521d371bc9b636b92abafd8bfce15045395
saidinasibu <adjinasibusaidi@gmail.com>
2025-04-11 12:31:13 -0700

    Homework Updated

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment a1 = new Assignment("Tonga Mpemba", "Division");
+        Console.WriteLine(a1.GetSummary());
+
+
+        MathAssignment a2 = new MathAssignment("Alfani Nfwamba", "Multiplications", "7.3", "9-11");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Nancy Makanda", "African History", "The Causes of Drc War ");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
