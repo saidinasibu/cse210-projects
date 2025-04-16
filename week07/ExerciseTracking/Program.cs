476ecf95c6be42be6da5db27e8bd467c12f6abaf
saidinasibu <adjinasibusaidi@gmail.com>
2025-04-15 23:01:10 -0700

    Exercise Tracking Program Updated

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Activity running = new Running(4.0, 32);
-        Activity cycling = new Cycling(26.0, 40);
-        Activity swimming = new Swimming(37, 35);
+        Activity running = new Running(4.0, 35);
+        Activity cycling = new Cycling(25.0, 50);
+        Activity swimming = new Swimming(35, 30);
 
         Console.WriteLine(running.GetSummary());
         Console.WriteLine(cycling.GetSummary());
